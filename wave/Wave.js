9f0ad2a49af5cb7eed28acf0cd96e7e4e540219d
hanhyung6376 <hanhyung6376@naver.com>
2021-10-07 22:39:58 +0900

    .

--- wave/Wave.js
+++ wave/Wave.js
@@ -39,29 +39,41 @@ export class Wave {
   }
 
   draw(ctx) {
+
+    ctx.beginPath();
+    let prevX = this.points[0].x;
+    let prevY = this.points[0].y;
+
+    ctx.moveTo(prevX, prevY);
+
     for (let i = 0; i < this.numberOfPoints; i++) {
+
       /*
-      그리기의 경로를 시작하는 메소드
-      https://developer.mozilla.org/ko/docs/Web/HTML/Canvas/Tutorial/Drawing_shapes
-      https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/beginPath
+      * 각 좌표에 곡선 연결
       */
-      ctx.beginPath();
+     const cx = (prevX + this.points[i].x) / 2;
+     const cy = (prevY + this.points[i].y) / 2;
 
-      /* 
-      호(arc)를 그리는 메소드를 이용하여 원을 그림 
-      2 * Math.PI = 반지름 * 2 = 지름
-      */
-      ctx.arc(this.points[i].x, this.points[i].y, 30, 0, 2 * Math.PI);
+     ctx.quadraticCurveTo(prevX, prevY, cx, cy);
 
-      /* 색상 RED & 채우기 */
-      ctx.fillStyle = "#ff0000";
-      ctx.fill();
-      ctx.closePath();
+     // 이전 좌표 갱신
+     prevX = this.points[i].x;
+     prevY = this.points[i].y;
 
       /* 공의 위치 변경하기 */
       if (i != 0 && i != this.numberOfPoints - 1) {
         this.points[i].update();
       }
     }
+
+      ctx.lineTo(prevX, prevY);
+      ctx.lineTo(this.stageWidth, this.stageWidth);
+      ctx.lineTo(0, this.stageHeight);
+      ctx.lineTo(this.points[0].x, this.points[0].y);
+
+      /* 색상 채우기 */
+      ctx.fillStyle = "blue";
+      ctx.fill();
+      ctx.closePath();
   }
 }
